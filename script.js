e968ab3e07cbbddcfba6004cede3e1da6fda636f
akshay agney <akshayagneyy41@gmail.com>
2024-06-07 12:33:10 +0530

    completed page 3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.main'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
 let fixedImg = document.querySelector(".fixed-image")
 let elementContainer = document.querySelector(".element-container")
 
