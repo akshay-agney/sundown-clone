5356c091cebb17d12ab2b4ccf2c5400019f859d2
akshay agney <akshayagneyy41@gmail.com>
2024-06-07 06:55:56 +0530

    random image effect

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true
+// });
+let fixedImg = document.querySelector(".fixed-image")
+let elementContainer = document.querySelector(".element-container")
+
+elementContainer.addEventListener('mouseenter',()=>{
+    fixedImg.style.display = "block"
+})
+elementContainer.addEventListener('mouseleave',()=>{
+    fixedImg.style.display = "none"
+})
+
+let elems = document.querySelectorAll(".elem");
+
+elems.forEach((ele)=>{
+    ele.addEventListener(`mouseenter`,()=>{
+            let images = ele.getAttribute("data-image");
+            fixedImg.style.backgroundImage = `url(${images})`
+        })
+})
+
+//
