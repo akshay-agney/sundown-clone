6e64e7800e856452066f76397df609065c28bf2c
akshay agney <akshayagneyy41@gmail.com>
2024-07-02 21:40:43 +0530

    almost done

--- script.js
+++ script.js
@@ -21,4 +21,17 @@ elems.forEach((ele)=>{
         })
 })
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 //
